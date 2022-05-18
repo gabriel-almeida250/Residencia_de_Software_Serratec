f1b47c0a1ccd55ffa0c6383812e8d19bc90df2ee
Gabriel Almeida <gabrielalmeida2612@outlook.com>
2022-05-18 12:03:29 -0300

    atualizando a api dia 18 parte 2

--- Módulo 05. Desenvolvimento de API Restful/WorkspacesEclipse/aulasAPI/1_api/src/main/java/com/residencia/firstapi/repository/AutorRepository.java
+++
@@ -1,9 +0,0 @@
-package com.residencia.firstapi.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.residencia.firstapi.entity.Autor;
-
-public interface AutorRepository extends JpaRepository<Autor, Integer>{
-
-}
