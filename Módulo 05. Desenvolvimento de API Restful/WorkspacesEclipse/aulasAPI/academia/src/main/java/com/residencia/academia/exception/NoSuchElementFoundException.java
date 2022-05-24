614b8c226fa93f47d8bd2ce5291cacaa9b9ccba9
Gabriel Almeida <gabrielalmeida2612@outlook.com>
2022-05-24 11:19:24 -0300

    atualizando a api dia 24. Começando a colocar as exceções no instrutor e na atividade

--- Módulo 05. Desenvolvimento de API Restful/WorkspacesEclipse/aulasAPI/academia/src/main/java/com/residencia/academia/exception/NoSuchElementFoundException.java
+++ Módulo 05. Desenvolvimento de API Restful/WorkspacesEclipse/aulasAPI/academia/src/main/java/com/residencia/academia/exception/NoSuchElementFoundException.java
@@ -1,5 +1,6 @@
 package com.residencia.academia.exception;
 
+@SuppressWarnings("serial")
 public class NoSuchElementFoundException extends RuntimeException {
 
 	public NoSuchElementFoundException(String message) {
